c277027302824a6ed5037e91ac121e6517c2ad44
abolfazl <realabolfazl21@gmail.com>
2020-09-18 16:57:36 +0430

    Add mustache lib

--- public/js/index.js
+++ public/js/index.js
@@ -17,25 +17,27 @@
      })
       
 
-     socket.on('newMessage' , function(message){
-         console.log('New Message' ,message)
-
-   
-         var li=jQuery('<li></li>')
-         li.text(`${message.from} : ${message.text}`)
-              
-         jQuery('#messages').append(li)
+     socket.on('newMessage', function(message){
+        var formatedTime = moment(message.createdAt).format('hh:mm a')
+        var template = jQuery('#message-template').html()  
+        var html = Mustache.render(template, {
+            text: message.text,
+            from: message.from,
+            createdAt: formatedTime
+        })
+       
+         jQuery('#messages').append(html)
      })
    
       
      jQuery('#message-from').on('submit' , function(e){
          e.preventDefault();
-                var message = jQuery('[name=message]');  
+         var message = jQuery('[name=message]');  
          socket.emit('creatMessage' , {
              from: 'User' , 
              text: message.val()
          } , function(){
-            jQuery('[name=message]').val('')
+           message.val('')
         })
      })
   
@@ -47,8 +49,6 @@ loactionButton.on('click', function(){
         return alert('Geolocation not supported by your browser');
     }
 
-
-     
     loactionButton.attr('disabled', 'disabled').text('Sending Location ...')
     navigator.geolocation.getCurrentPosition(function(position){
         loactionButton.removeAttr('disabled').text('Send Location');
@@ -64,11 +64,15 @@ loactionButton.on('click', function(){
 
 
 socket.on('newLocationMessage' , function(message){
-    var li =jQuery('<li></li>')
-    var a =jQuery('<a target="_blank">My Current Location</a>')
+    var formatedTime = moment(message.createdAt).format('hh:mm a')
 
-    li.text(`${message.from}: `)
-    a.attr('href' , message.url);
-    li.append(a);
-    jQuery('#messages').append(li)
+
+    var template = jQuery('#location-message-template').html()  
+    var html = Mustache.render(template, {
+        url: message.url,
+        from: message.from,
+        createdAt: formatedTime
+    })
+
+    jQuery('#messages').append(html)
 })
